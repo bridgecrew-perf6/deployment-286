9cbc533656e6fba60438e8f5764dc8f367504be8
Jaewon Moon <jaewonmoon@Jaewons-MacBook-Pro.local>
2022-01-19 19:59:44 -0500

    to fix the import error; there was some library imported wrong

--- deployment/settings.py
+++ deployment/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = "django-insecure-vg_zf5r9dhn--0600xo)_fc0a+3f13z7#gzq57xv83o7c^mmop"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
